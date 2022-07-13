f57e90a87eb9ed763704b47994318ab5607f2785
OrthodoxWindows <100134023+OrthodoxWindows@users.noreply.github.com>
2022-07-13 13:48:15 +0200

    Update DWM_Exemple_FR.bat

--- Samples-scripts/DWM_Exemple_FR.bat
+++ Samples-scripts/DWM_Exemple_FR.bat
@@ -13,20 +13,20 @@ if "%errorlevel%"=="1" echo Auteur : OrthodoxWin32 Type de thème : DWM Paramèt
 if "%errorlevel%"=="2" REM pause
 
 set /P c=Voulez-vous appliquer le thème d'exemple ?[Y\N]?
-if /I "%c%" EQU "Y" (Start C:\ThemeSwitcher\Window~2\ThemeSwitcher.exe Exemple.theme
+if /I "%c%" EQU "Y" (Start %userprofile%\TSS\ThemeSwitcher\ThemeSwitcher.exe Exemple.theme
 goto :CTT)
 if /I "%c%" EQU "N" (goto :ANNULATION)
 
 :CTT
-if exist C:\Classic\ClassicThemeTray.exe (ren "C:\Classic\ClassicThemeTray.exe" ClassicThemeTray_old.exe
+if exist %userprofile%\TSS\classic\classicthemetray.exe (ren "%userprofile%\classic\TSS\classicthemetray.exe" ClassicThemeTray_old.exe
 echo Le thème classique a été désactivé) else (echo Le thème classique est déjà désactivé)
 
-start C:\Classic\ClassicThemeTray_old.exe /disable
+start %userprofile%\TSS\classic\classicthemetray_old.exe /disable
 
-Set BT2=BasicThemer2.exe
-tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im BasicThemer2.exe
-schtasks.exe /change /tn basictheme /DISABLE
-echo Le thème basic a été désactivé) || echo Le thème basic est déjà désactivé
+Set BT2=basicthemer2.exe
+tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im BasicThemer2.exe) || echo BasicThemer2 n'est pas en court d'exécution
+if exist %userprofile%\tss\basic\basicthemer2.exe (ren "%userprofile%\tss\basic\basicthemer2.exe" BasicThemer2_old.exe
+echo Le thème basic a été désactivé) else (echo Le thème basic est déjà désactivé)
 
 set /P c=Voulez-vous redémarrer l'explorateur ?[Y\N]?
 if /I "%c%" EQU "Y" (TASKKILL /f /im explorer.exe
