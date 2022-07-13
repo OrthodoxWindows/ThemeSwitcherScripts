05959ea78d9309e64aebc3debeba1b9cac780d73
OrthodoxWindows <100134023+OrthodoxWindows@users.noreply.github.com>
2022-07-13 13:46:21 +0200

    Update Classic_Exemple - EN.bat

--- Samples-scripts/Classic_Exemple - EN.bat
+++ Samples-scripts/Classic_Exemple - EN.bat
@@ -13,20 +13,20 @@ if "%errorlevel%"=="1" echo Author : OrthodoxWin32 Theme type : classic Defined 
 if "%errorlevel%"=="2" REM pause
 
 set /P c=Do you want apply the sample theme ?[Y\N]?
-if /I "%c%" EQU "Y" (Start C:\ThemeSwitcher\Window~2\ThemeSwitcher.exe Exemple.theme
+if /I "%c%" EQU "Y" (Start %userprofile%\TSS\ThemeSwitcher\ThemeSwitcher.exe Exemple.theme
 goto :CTT)
 if /I "%c%" EQU "N" (goto :ANNULATION)
 
 :CTT
-if exist C:\Classic\ClassicThemeTray.exe (echo The classic theme is already active) else (ren "C:\Classic\ClassicThemeTray_old.exe" ClassicThemeTray.exe
+if exist %userprofile%\tss\classic\classicthemetray.exe (echo The classic theme is already active) else (ren "%userprofile%\tss\classic\classicthemetray_old.exe" classicthemetray.exe
 echo The classic theme has been activated)
 
 Set BT2=basicthemer2.exe
-tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im BasicThemer2.exe
-schtasks.exe /change /tn basictheme /DISABLE
-echo The basic theme has been disabled) || echo The basic theme is already disabled
+tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im BasicThemer2.exe) || echo 0
+if exist %userprofile%\tss\basic\basicthemer2.exe (ren "%userprofile%\tss\basic\basicthemer2.exe" BasicThemer2_old.exe
+echo The basic theme has been disabled) else (echo The basic theme is already disabled)
 
-start C:\Classic\ClassicThemeTray.exe /enable
+start %userprofile%\TSS\classic\classicthemetray.exe /enable
 
 set /P c=Do you want to restart explorer ?[Y\N]?
 if /I "%c%" EQU "Y" (TASKKILL /f /im explorer.exe
