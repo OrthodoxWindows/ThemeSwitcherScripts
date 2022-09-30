da172ec5edbab730a123006fb77d8f1ffab68f31
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2022-10-01 01:03:16 +0200

    Add files via upload

---
+++ Automatic_Scripts_No_Explorer_restart/DWM-Effects_EN.bat
@@ -0,0 +1,16 @@
+@echo off
+chcp 65001 > nul
+
+if exist "%userprofile%"\TSS\classic\classicthemetray.exe (ren "%userprofile%\TSS\classic\classicthemetray.exe" classicthemetray_old.exe
+echo The classic theme has been disabled) else (echo The classic theme is already disabled)
+
+start "" /D "%USERPROFILE%\TSS\classic" classicthemetray_old.exe /disable
+
+Set BT2=basicthemer2.exe
+tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im basicthemer2.exe) || echo INA
+if exist "%userprofile%"\tss\basic\basicthemer2.exe (ren "%userprofile%\tss\basic\basicthemer2.exe" basicthemer2_old.exe
+echo The basic theme has been disabled) else (echo The basic theme is already disabled)
+
+:MSG
+echo Task completed
+End
---
+++ Automatic_Scripts_No_Explorer_restart/DWM-Effets_FR.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 > nul
+
+:CTT
+if exist "%userprofile%"\TSS\classic\classicthemetray.exe (ren "%userprofile%\TSS\classic\classicthemetray.exe" classicthemetray_old.exe
+echo Le thème classique a été désactivé) else (echo Le thème classique est déjà désactivé)
+
+start "" /D "%USERPROFILE%\TSS\classic" classicthemetray_old.exe /disable
+
+Set BT2=basicthemer2.exe
+tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im basicthemer2.exe) || echo INA
+if exist "%userprofile%"\tss\basic\basicthemer2.exe (ren "%userprofile%\tss\basic\basicthemer2.exe" basicthemer2_old.exe
+echo Le thème basic a été désactivé) else (echo Le thème basic est déjà désactivé)
+
+:MSG
+echo Tâche terminée
+End
