d834cbb81a702472096cc9a7001f74564f4616d7
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-07-20 14:59:24 +0200

    Update Classique_FR.bat

--- Automatic_Scripts_No_Explorer_restart/Classique_FR.bat
+++ Automatic_Scripts_No_Explorer_restart/Classique_FR.bat
@@ -4,13 +4,16 @@ chcp 65001 > nul
 if exist "%userprofile%"\tss\classic\classicthemetray.exe (echo Le thème classique est déjà activé) else (ren "%userprofile%\tss\classic\classicthemetray_old.exe" classicthemetray.exe
 echo Le thème classique a été activé)
 
-Set BT2=basicthemer2.exe
-tasklist | find /i "%BT2%">nul  && (TASKKILL /f /im basicthemer2.exe) || echo INA
-if exist "%userprofile%"\tss\basic\basicthemer2.exe (ren "%userprofile%\tss\basic\basicthemer2.exe" basicthemer2_old.exe
+Set BT5=uxsms.exe
+tasklist | find /i "%BT5%">nul  && (TASKKILL /f /im uxsms.exe) || goto :1
+:1
+if exist "%userprofile%"\tss\basic\uxsms.exe (ren "%userprofile%\tss\basic\uxsms.exe" uxsms_old.exe
 echo Le thème basic a été désactivé) else (echo Le thème basic est déjà désactivé)
 
 start "" /D "%USERPROFILE%\TSS\classic" classicthemetray.exe /enable
 
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 0
+
 :MSG
 echo Tâche terminée
-End
