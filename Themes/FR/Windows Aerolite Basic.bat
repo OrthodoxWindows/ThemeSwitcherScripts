00fe471bfc97cc666081b38ac1258a6d365fa618
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-07-23 12:43:43 +0200

    Add files via upload

---
+++ Themes/FR/Windows Aerolite Basic.bat
@@ -0,0 +1,43 @@
+@echo off
+chcp 65001 > nul
+
+echo ************************************************************
+echo *               Windows 10 Aerolite Basique                *
+echo *                                                          *
+echo * Appuyer sur I pour plus d´informations, C pour continuer *
+echo ************************************************************
+echo.
+
+CHOICE /C IC
+if "%errorlevel%"=="1" goto :INF
+if "%errorlevel%"=="2" goto :APPLY
+
+:INF
+echo.
+echo **************************************************************************************************
+echo * Auteur : Equipe de design de Microsoft                                                         *
+echo * Type de thème : Basic, style visuel Windows 10 Aerolite                                        *
+echo * Paramètres définis : Il s´agit d'un thème similaire au thème Aero par défault dans Windows 10, *
+echo * mais avec le style visuel Aerolite et en mode Basique                                          *
+echo **************************************************************************************************    
+echo.
+
+:APPLY
+set /P c=Voulez-vous appliquer le thème Windows 10 Aerolite Basique ?[Y\N]?
+if /I "%c%" EQU "Y" (call "%userprofile%\TSS\Switcher_Scripts\Basic_FR.bat"
+goto :THM)
+if /I "%c%" EQU "N" (goto :ABORT)
+
+:THM
+"%userprofile%\TSS\WTS\ThemeSwitcher.exe" "C:\Windows\Resources\Themes\aerolite.theme"
+
+start "" /D "%userprofile%\TSS\CTC" "Themes_W10.exe" @appearance /Apply "Win10 - Aerolite" "Normal"
+
+call "%userprofile%\TSS\Explorer_Restart\Explorer_Restart_FR.bat"
+
+goto :END
+
+:ABORT opération annulée
+pause
+
+:END
