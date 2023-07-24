3aa314495eb836063695cf2894cc1007c9d05078
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-07-24 20:23:38 +0200

    Add files via upload

---
+++ Themes/EN/High Contrast #1.bat
@@ -0,0 +1,42 @@
+@echo off
+chcp 65001 > nul
+
+echo ************************************************************
+echo *               Windows 10 High Contrast #1                *
+echo *                                                          *
+echo *       Press I for more information, C to continue        *
+echo ************************************************************
+echo.
+
+CHOICE /C IC
+if "%errorlevel%"=="1" goto :INF
+if "%errorlevel%"=="2" goto :APPLY
+
+:INF
+echo.
+echo ************************************************************************************
+echo * Author : Microsoft design team                                                   *
+echo * Theme type : DWM-Effect, Windows 10 Aerolite HC Visual style                     *
+echo * Parameters set : This is a high contrast theme included by default in Windows 10 *
+echo ************************************************************************************
+echo.
+
+:APPLY
+set /P c=Do you want to apply Windows 10 High Contrast #1 theme ?[Y\N]?
+if /I "%c%" EQU "Y" (call "%userprofile%\TSS\Switcher_Scripts\DWM-Effects.bat"
+goto :THM)
+if /I "%c%" EQU "N" (goto :ABORT)
+
+:THM
+"%userprofile%\TSS\WTS\ThemeSwitcher.exe" "C:\Windows\Resources\Ease of Access Themes\hc1.theme"
+
+start "" /D "%userprofile%\TSS\CTC" "Themes_W10.exe" @appearance /Apply "Win10 - High Contrast #1" "Normal"
+
+call "%userprofile%\TSS\Explorer_Restart\Explorer_Restart.bat"
+
+goto :END
+
+:ABORT operation canceled
+pause
+
+:END
