1e86e827efb12d2c1b6512bc214efd58d2650f32
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-07-25 13:27:09 +0200

    Create Classic.bat

---
+++ Switcher_Scripts/FR/Classic.bat
@@ -0,0 +1,19 @@
+@echo off
+chcp 65001 > nul
+
+if exist "%userprofile%"\tss\classic\classicthemetray.exe (echo Le thème classique est déjà activé) else (ren "%userprofile%\tss\classic\classicthemetray_old.exe" classicthemetray.exe
+echo Le thème classique a été activé)
+
+Set BT5=uxsms.exe
+tasklist | find /i "%BT5%">nul  && (TASKKILL /f /im uxsms.exe) || goto :1
+:1
+if exist "%userprofile%"\tss\basic\uxsms.exe (ren "%userprofile%\tss\basic\uxsms.exe" uxsms_old.exe
+echo Le thème basic a été désactivé) else (echo Le thème basic est déjà désactivé)
+
+start "" /D "%USERPROFILE%\TSS\classic" classicthemetray.exe /enable
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 0
+
+:MSG
+echo Tâche terminée
