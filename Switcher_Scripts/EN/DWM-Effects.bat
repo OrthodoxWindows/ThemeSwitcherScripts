1ff5fdbfd74efee5658db11b1c77ff25f742d18c
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-07-27 23:58:52 +0200

    Update DWM-Effects.bat

--- Switcher_Scripts/EN/DWM-Effects.bat
+++ Switcher_Scripts/EN/DWM-Effects.bat
@@ -12,8 +12,26 @@ tasklist | find /i "%BT5%">nul  && (TASKKILL /f /im uxsms.exe) || goto :1
 if exist "%userprofile%"\tss\basic\uxsms.exe (ren "%userprofile%\tss\basic\uxsms.exe" uxsms_old.exe
 echo The basic theme has been disabled) else (echo The basic theme is already disabled)
 
+if exist "%userprofile%"\tss\basic\uxsms.exe (goto :2) else (ren "%userprofile%\tss\basic\uxsms_old.exe" uxsms.exe
+echo The basic theme has been activated)
+:2
+Set BT5=uxsms.exe
+tasklist | find /i "%BT5%">nul  && echo The basic theme is already active || (start "" /D "%USERPROFILE%\TSS\Basic" uxsms.exe)
+
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 1
+if exist "%userprofile%"\tss\Task-Scripts\@DDEF.bat" (ren "%userprofile%"\tss\Task-Scripts\@DDEF.bat" @DDEF_old.bat
+echo DWM extended frames have been enabled) else (echo DWM Extended Frames are already enabled)
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /t REG_DWORD /d 1
+if exist "%userprofile%"\tss\Task-Scripts\@UT.bat" (ren "%userprofile%"\tss\Task-Scripts\@UT.bat" @UT_old.bat
+echo Composition in client area has been enabled) else (echo Composition in client area is already enabled)
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /t REG_DWORD /d 1
+if exist "%userprofile%"\tss\Task-Scripts\@UTNC.bat" (ren "%userprofile%"\tss\Task-Scripts\@UTNC.bat" @UTNC_old.bat
+echo Full composition in the client area has been enabled) else (echo Full composition in the client area is already enabled)
 
 :MSG
 echo Task completed
