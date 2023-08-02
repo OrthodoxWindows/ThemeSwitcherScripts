43a0f32dc4d3a9fa624f89f57f7dead646797304
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-03 00:21:18 +0200

    Create @CTEX.bat

---
+++ Task_Scripts/@CTEX.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001 > nul
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@classic-theme-explorer /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@classic-theme-explorer /v Disabled /t REG_DWORD /d 0
