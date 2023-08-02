c185b480e973e2136b74259f888cca9fbd51eaae
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-03 00:44:38 +0200

    Create UT.bat

---
+++ Task_Scripts/UT.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001 > nul
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\untheme /v Disabled /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\untheme /v Disabled /t REG_DWORD /d 0
