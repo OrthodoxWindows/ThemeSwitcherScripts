107b505e1feb253734acb0e590d4ef944564302c
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-03 00:43:57 +0200

    Update and rename @CTEX.bat to CTEX.bat

--- Task_Scripts/@CTEX.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 > nul
-
-REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@classic-theme-explorer /v Disabled /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@classic-theme-explorer /v Disabled /t REG_DWORD /d 0
