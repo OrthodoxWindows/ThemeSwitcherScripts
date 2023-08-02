51773d3b36f10792dccf86316582bf159bb8ff12
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-02 22:32:04 +0200

    Update DWM-Effects.bat

--- Switcher_Scripts/FR/DWM-Effects.bat
+++ Switcher_Scripts/FR/DWM-Effects.bat
@@ -14,18 +14,21 @@ echo Le thème basic a été désactivé) else (echo Le thème basic est déjà 
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%"\tss\Task-Scripts\@DDEF.bat" (ren "%userprofile%"\tss\Task-Scripts\@DDEF.bat" @DDEF_old.bat
+if exist "%userprofile%\tss\Task_Scripts\@DDEF.bat" (ren "%userprofile%\tss\Task_Scripts\@DDEF.bat" @DDEF_old.bat
 echo Les cadres étendus de DWM ont été activés) else (echo Les cadres étendus de DWM sont déjà activés)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%"\tss\Task-Scripts\@UT.bat" (ren "%userprofile%"\tss\Task-Scripts\@UT.bat" @UT_old.bat
+if exist "%userprofile%\tss\Task_Scripts\@UT.bat" (ren "%userprofile%\tss\Task_Scripts\@UT.bat" @UT_old.bat
 echo La composition dans la zone cliente a été activé) else (echo La composition dans la zone cliente est déjà activé)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%"\tss\Task-Scripts\@UTNC.bat" (ren "%userprofile%"\tss\Task-Scripts\@UTNC.bat" @UTNC_old.bat
+if exist "%userprofile%\tss\Task_Scripts\@UTNC.bat" (ren "%userprofile%\tss\Task_Scripts\@UTNC.bat" @UTNC_old.bat
 echo La composition intégrale dans la zone cliente a été activé) else (echo La composition intégrale dans la zone cliente est déjà activé)
+
+if exist "%userprofile%\tss\Task_Scripts\@CTEX.bat" (ren "%userprofile%\tss\Task_Scripts\@CTEX.bat" @CTEX_old.bat
+echo La barre des tâches classique à été désactivé) else (echo La barre des tâches classique est déjà désactivé)
 
 :MSG
 echo Tâche terminée
