001a6a0eb9e0813d6078d5a7c01307ea0071c363
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-02 22:30:46 +0200

    Update Classic.bat

--- Switcher_Scripts/FR/Classic.bat
+++ Switcher_Scripts/FR/Classic.bat
@@ -14,18 +14,21 @@ start "" /D "%USERPROFILE%\TSS\classic" classicthemetray.exe /enable
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 0
-if exist "%userprofile%"\tss\Task-Scripts\@DDEF.bat" (echo Les cadres étendus de DWM sont déjà désactivés) else (ren "%userprofile%"\tss\Task-Scripts\@DDEF_old.bat" @DDEF.bat
+if exist "%userprofile%\tss\Task_Scripts\@DDEF.bat" (echo Les cadres étendus de DWM sont déjà désactivés) else (ren "%userprofile%\tss\Task_Scripts\@DDEF_old.bat" @DDEF.bat
 echo Les cadres étendus de DWM ont été désactivés)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%"\tss\Task-Scripts\@UT.bat" (ren "%userprofile%"\tss\Task-Scripts\@UT.bat" @UT_old.bat
+if exist "%userprofile%\tss\Task_Scripts\@UT.bat" (ren "%userprofile%\tss\Task_Scripts\@UT.bat" @UT_old.bat
 echo La composition dans la zone cliente a été activé) else (echo La composition dans la zone cliente est déjà activé)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%"\tss\Task-Scripts\@UTNC.bat" (ren "%userprofile%"\tss\Task-Scripts\@UTNC.bat" @UTNC_old.bat
+if exist "%userprofile%\tss\Task_Scripts\@UTNC.bat" (ren "%userprofile%\tss\Task_Scripts\@UTNC.bat" @UTNC_old.bat
 echo La composition intégrale dans la zone cliente a été activé) else (echo La composition intégrale dans la zone cliente est déjà activé)
+
+if exist "%userprofile%\tss\Task_Scripts\@CTEX.bat" (echo La barre des tâches classique est déjà activé) else (ren "%userprofile%\tss\Task_Scripts\@CTEX_old.bat" @CTEX.bat
+echo La barre des tâches classique à été activé)
 
 :MSG
 echo Tâche terminée
