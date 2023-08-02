3d20f9dd5d9dd799d99a5cac5ed133494a1e9f7d
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2023-08-02 22:34:04 +0200

    Update DWM-Classic-IntegralNoComposition.bat

--- Switcher_Scripts/FR/DWM-Classic-IntegralNoComposition.bat
+++ Switcher_Scripts/FR/DWM-Classic-IntegralNoComposition.bat
@@ -13,22 +13,22 @@ if exist "%userprofile%"\tss\basic\uxsms.exe (ren "%userprofile%\tss\basic\uxsms
 echo Le thème basic a été désactivé) else (echo Le thème basic est déjà désactivé)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 0
-if exist "%userprofile%\tss\Task_Scripts\@DDEF.bat" (echo Les cadres étendus de DWM sont déjà désactivés) else (ren "%userprofile%\tss\Task_Scripts\@DDEF_old.bat" @DDEF.bat
-echo Les cadres étendus de DWM ont été désactivés)
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@disable-dwm-extended-frames /v Disabled /t REG_DWORD /d 1
+if exist "%userprofile%\tss\Task_Scripts\@DDEF.bat" (ren "%userprofile%\tss\Task_Scripts\@DDEF.bat" @DDEF_old.bat
+echo Les cadres étendus de DWM ont été activés) else (echo Les cadres étendus de DWM sont déjà activés)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /t REG_DWORD /d 0
-if exist "%userprofile%\tss\Task_Scripts\@UT.bat" (echo La composition dans la zone cliente est déjà désactivé) else (ren "%userprofile%\tss\Task_Scripts\@UT_old.bat" @UT.bat
-echo La composition dans la zone cliente a été désactivé)
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@untheme /v Disabled /t REG_DWORD /d 1
+if exist "%userprofile%\tss\Task_Scripts\@UT.bat" (ren "%userprofile%\tss\Task_Scripts\@UT.bat" @UT_old.bat
+echo La composition dans la zone cliente a été activé) else (echo La composition dans la zone cliente est déjà activé)
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /t REG_DWORD /d 1
-if exist "%userprofile%\tss\Task_Scripts\@UTNC.bat" (ren "%userprofile%\tss\Task_Scripts\@UTNC.bat" @UTNC_old.bat
-echo La composition intégrale dans la zone cliente a été désactivé) else (echo La composition intégrale dans la zone cliente est déjà désactivé)
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Windhawk\Engine\Mods\local@unthemed-nocomposition /v Disabled /t REG_DWORD /d 0
+if exist "%userprofile%\tss\Task_Scripts\@UTNC.bat" (echo La composition intégrale dans la zone cliente est déjà activé) else (ren "%userprofile%\tss\Task_Scripts\@UTNC_old.bat" @UTNC.bat
+echo La composition intégrale dans la zone cliente a été désactivé)
 
-if exist "%userprofile%\tss\Task_Scripts\@CTEX.bat" (ren "%userprofile%\tss\Task_Scripts\@CTEX.bat" @CTEX_old.bat
-echo La barre des tâches classique à été désactivé) else (echo La barre des tâches classique est déjà désactivé)
+if exist "%userprofile%\tss\Task_Scripts\@CTEX.bat" (echo La barre des tâches classique est déjà activé) else (ren "%userprofile%\tss\Task_Scripts\@CTEX_old.bat" @CTEX.bat
+echo La barre des tâches classique à été activé)
 
 :MSG
 echo Tâche terminée
