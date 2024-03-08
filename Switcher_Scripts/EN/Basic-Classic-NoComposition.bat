b0840a4ebac869a6b7d736f997b57b40132391a2
OrthodoxWin32 <100134023+OrthodoxWindows@users.noreply.github.com>
2024-03-08 01:40:13 +0100

    Add files via upload

---
+++ Switcher_Scripts/EN/Basic-Classic-NoComposition.bat
@@ -0,0 +1,25 @@
+@echo off
+chcp 65001 > nul
+
+if exist "%userprofile%"\tss\classic\classicthemetray.exe (ren "%userprofile%\tss\classic\classicthemetray.exe" classicthemetray_old.exe
+echo The classic theme has been disabled) else (echo The classic theme is already disabled)
+
+start "" /D "%USERPROFILE%\TSS\classic" classicthemetray_old.exe /disable
+
+if exist "%userprofile%"\tss\basic\uxsms.exe (goto :2) else (ren "%userprofile%\tss\basic\uxsms_old.exe" uxsms.exe
+echo The basic theme has been activated)
+:2
+Set BT5=uxsms.exe
+tasklist | find /i "%BT5%">nul  && echo The basic theme is already active || (start "" /D "%USERPROFILE%\TSS\Basic" uxsms.exe)
+
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Settings" /v "Metro" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Metro /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Classic" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Classic /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Classic-NoClassicScrollbar" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Classic-NoClassicScrollbar /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Style" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Style /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Basic" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Basic /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Classic-Integral" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Classic-Integral /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Basic-Classic" ^| find "0x0" ') do (REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Basic-Classic /t REG_DWORD /d 1 /f
+call "%userprofile%\tss\task_scripts\@BasicClassicMods.bat")
+
+:MSG
+echo Tâche terminée
---
+++ Switcher_Scripts/EN/Classic.bat
@@ -0,0 +1,25 @@
+@echo off
+chcp 65001 > nul
+
+if exist "%userprofile%"\tss\classic\classicthemetray.exe (echo The classic theme is already active) else (ren "%userprofile%\tss\classic\classicthemetray_old.exe" classicthemetray.exe
+echo The classic theme has been activated)
+
+Set BT5=uxsms.exe
+tasklist | find /i "%BT5%">nul  && (TASKKILL /f /im uxsms.exe) || goto :1
+:1
+if exist "%userprofile%"\tss\basic\uxsms.exe (ren "%userprofile%\tss\basic\uxsms.exe" uxsms_old.exe
+echo The basic theme has been disabled) else (echo The basic theme is already disabled)
+
+start "" /D "%USERPROFILE%\TSS\classic" classicthemetray.exe /enable
+
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Settings" /v "Metro" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Metro /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Basic" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Basic /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Basic-Classic" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Basic-Classic /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Style" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Style /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Classic-Integral" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Classic-Integral /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "DWM-Classic-NoClassicScrollbar" ^| find "0x1" ') do REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v DWM-Classic-NoClassicScrollbar /t REG_DWORD /d 0 /f
+for /f "tokens=3 " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles" /v "Classic" ^| find "0x0" ') do (REG ADD HKEY_CURRENT_USER\SOFTWARE\ThemeSwitcherScripts\Profiles /v Classic /t REG_DWORD /d 1 /f
+call "%userprofile%\tss\task_scripts\@ClassicMods.bat")
+
+:MSG
+echo Tâche terminée
